fec6e6e7ba93a03de0e47ec9e9677367ff8120f1
takamizawanoriaki <takamizawanoriaki@localhost>
2023-08-23 17:14:13 +0900

    initial commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class DemoController {
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num, Model model) {
+		//int sum = 0;
+		//for(int i = 1 ; i <= 10 ; i++) {
+		//	sum += i;
+		//}
+		//model.addAttribute("msg", "sum = " + sum);
+		model.addAttribute("msg", "sum = sum");
+		return "index";
+	}
+}
